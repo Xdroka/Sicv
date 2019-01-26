067e08f1c6a56c729673e6952252dc4ed7aadcdb
Ítalo Jorge de Carvalho Manhães <italomanhaes@ioasys.com.br>
2019-01-26 19:20:45 -0200

    teste com cloud firestore

--- app/src/main/java/com/tcc/sicv/ui/SignUpActivity.java
+++ app/src/main/java/com/tcc/sicv/ui/SignUpActivity.java
@@ -1,15 +1,46 @@
 package com.tcc.sicv.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.tcc.sicv.R;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SignUpActivity extends AppCompatActivity {
+
+    private String TAG = "aqui";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        DocumentReference db = FirebaseFirestore.getInstance().document("usuarios/pedro");
+
+        // Create a new user with a first and last name
+        Map<String, Object> user = new HashMap<>();
+        user.put("email", "pedro");
+        user.put("senha", "pedro01");
+
+// Add a new document with a generated ID
+        db.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                Log.d("Aqui", "Sucesso");
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("Aqui", "Deu ruim");
+            }
+        });
     }
 }
