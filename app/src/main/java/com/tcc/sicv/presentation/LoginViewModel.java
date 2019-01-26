1a5b14c8db7bd785400a0be292aac07c98c8cd61
=Pedro Henrique <=pedrooliveira@ioasys.com.br>
2019-01-26 16:18:16 -0200

    WIP LoginViewModel

--- app/src/main/java/com/tcc/sicv/presentation/LoginViewModel.java
+++ app/src/main/java/com/tcc/sicv/presentation/LoginViewModel.java
@@ -9,6 +9,10 @@ import com.tcc.sicv.presentation.model.ViewState;
 
 import java.util.regex.Pattern;
 
+import static com.tcc.sicv.presentation.model.State.EMPTY;
+import static com.tcc.sicv.presentation.model.State.INVALID;
+import static com.tcc.sicv.presentation.model.State.VALID;
+
 public class LoginViewModel extends ViewModel {
     private MutableLiveData<ViewState<User>> viewState;
     private User user;
@@ -24,24 +28,31 @@ public class LoginViewModel extends ViewModel {
     public void sigin(String email, String password){
         user.setEmail(email);
         user.setPassword(password);
-
-
-
     }
 
-    private boolean validateEmail(String email) {
+    private void validateEmail(String email) {
         String regex = "^[\\w-\\+]+(\\.[\\w]+)*@[\\w-]+(\\.[\\w]+)*(\\.[a-z]{2,})$";
         Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
-        return pattern.matcher(email).find();
+        if(pattern.matcher(email).find()){ emailState.postValue(VALID); }
+        else {
+            if(email.isEmpty()) emailState.postValue(EMPTY);
+            else emailState.postValue(INVALID);
+        }
     }
 
-    private boolean validatePassword(String password){
-        return password.length() >= 6;
+    private void validatePassword(String password){
+        if(password.length() >= 6) passwordState.postValue(VALID);
+        else {
+            if(password.isEmpty()) passwordState.postValue(EMPTY);
+            else passwordState.postValue(INVALID);
+        }
     }
 
-    public LiveData<ViewState<User>> getViewState() {
-        return viewState;
-    }
+    public LiveData<ViewState<User>> getViewState() { return viewState; }
+
+    public LiveData<State> getEmailState() { return emailState; }
+
+    public LiveData<State> getPasswordState() { return passwordState; }
 
     @Override
     protected void onCleared() {
--- app/src/main/java/com/tcc/sicv/ui/LoginActivity.java
+++ app/src/main/java/com/tcc/sicv/ui/LoginActivity.java
@@ -1,18 +1,25 @@
 package com.tcc.sicv.ui;
 
+import android.arch.lifecycle.Observer;
+import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.tcc.sicv.R;
 import com.tcc.sicv.presentation.LoginViewModel;
+import com.tcc.sicv.presentation.model.User;
+import com.tcc.sicv.presentation.model.ViewState;
 
 public class LoginActivity extends AppCompatActivity {
     private LoginViewModel mViewModel;
     private Button button = findViewById(R.id.signButton);
     private TextInputEditText emailEt = findViewById(R.id.emailEt);
     private TextInputEditText passwordEt = findViewById(R.id.passwordEt);
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +27,30 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         mViewModel = new LoginViewModel();
         creatingObservers();
+        mAuth = FirebaseAuth.getInstance();
     }
 
     private void creatingObservers(){
-//        mViewModel.g
+        mViewModel.getViewState().observe(this, new Observer<ViewState<User>>() {
+            @Override
+            public void onChanged(@Nullable ViewState<User> userViewState) {
+                handleWithMainFlow(userViewState);
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sigInFlow();
+            }
+        });
+    }
+
+    private void sigInFlow() {
+
+    }
+
+    private void handleWithMainFlow(ViewState<User> userViewState) {
+
     }
 
 }
