f5e192a2746ab6607fa394ec3438944c3166e093
Ítalo Jorge de Carvalho Manhães <italomanhaes@ioasys.com.br>
2019-03-05 12:00:34 -0300

    criação layout do perfil

--- app/src/main/java/com/tcc/sicv/ui/ProfileActivity.java
+++ app/src/main/java/com/tcc/sicv/ui/ProfileActivity.java
@@ -1,15 +1,30 @@
 package com.tcc.sicv.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.tcc.sicv.R;
+import com.tcc.sicv.base.BaseActivity;
 
-public class ProfileActivity extends AppCompatActivity {
-
+public class ProfileActivity extends BaseActivity {
+    TextView nameTv;
+    TextView cpfTv;
+    TextView emailTv;
+    TextView dateTv;
+    TextView telTv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        setupToolbar(R.id.main_toolbar,R.string.user_profile,true);
+        setupViews();
+    }
+
+    private void setupViews() {
+        nameTv = findViewById(R.id.nameTv);
+        cpfTv = findViewById(R.id.cpfTv);
+        emailTv = findViewById(R.id.emailTv);
+        dateTv = findViewById(R.id.dateTv);
+        telTv = findViewById(R.id.telephoneTv);
     }
 }
