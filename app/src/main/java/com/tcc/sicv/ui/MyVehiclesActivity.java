252e190ee523f94d3c077e4ececf399b9b931ebe
Pedro <pedro.fsa_21@hotmail.com>
2019-03-04 21:50:19 -0300

    creating layout of my vehicles

---
+++ app/src/main/java/com/tcc/sicv/ui/MyVehiclesActivity.java
@@ -0,0 +1,15 @@
+package com.tcc.sicv.ui;
+
+import android.os.Bundle;
+
+import com.tcc.sicv.R;
+import com.tcc.sicv.base.BaseActivity;
+
+public class MyVehiclesActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_vehicles);
+    }
+}
